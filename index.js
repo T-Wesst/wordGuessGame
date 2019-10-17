1fc6b3a82d777d10684b69b65b75cb0c13633244
T-Wesst <code.daddy17@gmail.com>
2019-10-17 01:17:04 -0700

    set acceptable range of characters, check if game is over

--- index.js
+++ index.js
@@ -12,6 +12,24 @@
   let answer = new Array(randomWord.length).fill('_');
   updateUI();
 
+  document.onkeydown = function(event) {
+    // if key pressed is within range from a-z
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+      let userInput = event.key.toLowerCase();
+      console.log(userInput);
+      // if the game is over
+      if (remainingGuesses === 0) {
+        losses++;
+        reset();
+      } else {
+        checkInput(randomWord, userInput);
+        updateUI();
+      }
+    } else {
+      alert('sorry, please enter a character between a-z');
+    }
+  };
+
   // UI
   function updateUI() {
     console.log(randomWord, randomWord.length);
