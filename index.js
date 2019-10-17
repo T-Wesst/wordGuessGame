19a87c1dab4e31a2852ec29861aee61f962d52e7
T-Wesst <code.daddy17@gmail.com>
2019-10-17 01:03:15 -0700

    set scorekeepers and create answer array

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let url =
-  'https://cors-anywhere.herokuapp.com/http://app.linkedin-reach.io/words';
-
 (async () => {
+  let url =
+    'https://cors-anywhere.herokuapp.com/http://app.linkedin-reach.io/words';
   const { data } = await axios.get(url);
   const arrWords = data.split('\n');
   let randomWord = arrWords[Math.floor(Math.random() * arrWords.length)];
+  // GAME TRACKERS
+  let wins = 0;
+  let losses = 0;
+  let guessedLetters = [];
+  let remainingGuesses = randomWord.length;
+  let answer = new Array(randomWord.length).fill('_');
+  console.log(answer);
+  console.log(remainingGuesses);
   console.log(randomWord);
 })();
